e204b948d71340a7b4683a33a9d3bf871d73ccf1
kidc1412 <kidc1412x@gmail.com>
2018-06-27 23:27:03 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -25,67 +25,65 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<Widget> widgets = [];
+  List<int> items = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    for (int i = 0; i < 10; i++) {
+      widgets.add(buildTile(i));
+      items.add(i);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> widgets = [];
-    List<int> items = [];
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Sample App"),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.add), onPressed: _details),
+        ],
+      ),
+      body: new ListView.builder(
+          padding: new EdgeInsets.all(10.0),
+          itemCount: widgets.length, //规定列表显示的数量 如果不设置则为无限数量
+          itemBuilder: (BuildContext context, int i){
+            return buildTile(i);
+          },
+      ),
+    );
+  }
 
-    @override
-    void initState() {
-      // TODO: implement initState
-      super.initState();
-      for (int i = 0; i < 10; i++) {
-        widgets.add(getRow(i));
-        items.add(i);
-      }
-    }
-
-    @override
-    Widget build(BuildContext context) {
-      return new Scaffold(
-        appBar: new AppBar(
-          title: new Text("Sample App"),
-          actions: <Widget>[
-            new IconButton(icon: new Icon(Icons.list, color: Colors.white,), onPressed: _details),
-          ],
-        ),
-        body: new ListView.builder(
-            padding: new EdgeInsets.all(10.0),
-            itemCount: widgets.length, //规定列表显示的数量 如果不设置则为无限数量
-            itemBuilder: (BuildContext context, int i){
-              buildTile(i)
-            }
-        ),
-      );
-    }
     
-    Widget buildTile(int i) {
-      return new GestureDetector(
+  Widget buildTile(int i) {
+    return new GestureDetector(
 
-        child: new Column(
-          children: <Widget>[
+      child: new Column(
+        children: <Widget>[
 
-            new ListTile(
-                title: new Text("Row is $i"),
-                subtitle: new Text("Row is $i"),
-                trailing: new Icon(Icons.keyboard_arrow_right,color: Colors.grey,),
-                leading: new CircleAvatar(child: new Text(i.toString()))
-            ),
-            //增加分割线
-            _setDivider(i),
-          ],
-        ),
-        //列表item的点击事件
-        onTap: () {
-          setState(() {
-            widgets = new List.from(widgets);
-            widgets.add(buildTile(widgets.length + 1)); //点击item就增加一行数据
-            print('row $i');
-          });
-        },
-      );
-    }
+          new ListTile(
+              title: new Text("Row is $i"),
+              subtitle: new Text("Row is $i"),
+              trailing: new Icon(Icons.keyboard_arrow_right,color: Colors.grey,),
+              leading: new CircleAvatar(child: new Text(i.toString()))
+          ),
+          //增加分割线
+          _setDivider(i),
+        ],
+      ),
+      //列表item的点击事件
+      onTap: () {
+        setState(() {
+          widgets = new List.from(widgets);
+          widgets.add(buildTile(widgets.length + 1)); //点击item就增加一行数据
+          print('row $i');
+        });
+      },
+    );
+  }
     
     //设置最后一条数据不显示分割线
   _setDivider(int i){
@@ -120,4 +118,5 @@ class _MyHomePageState extends State<MyHomePage> {
       )
     );
   }
+
 }
